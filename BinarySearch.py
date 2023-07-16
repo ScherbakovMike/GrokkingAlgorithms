a8edeac175f1df2e187d50a1a2dc99410a452f73
Mike Scherbakov <scherbakov.mike@gmail.com>
2023-07-16 09:13:15 +0300

    Added comment

--- BinarySearch.py
+++ BinarySearch.py
@@ -14,19 +14,23 @@ While low<=high and array[mid]!=desiredValue:
     else
         low = mid+1
 """
-sortedArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 19]
-low = 0
-high = len(sortedArray)-1
-mid = int((high+low)/2)
-found = False
-searchItem = 4
-while low <= high and sortedArray[mid] != searchItem:
-    if (sortedArray[mid] > searchItem):
-        high = mid-1
-    else:
-        low = mid+1
+
+def binarySearch(sortedArray, searchItem):
+    low = 0
+    high = len(sortedArray)-1
     mid = int((high+low)/2)
-if sortedArray[mid] == searchItem:
-    print(sortedArray[mid])
-else:
-    print(None)
+    while low <= high and sortedArray[mid] != searchItem:
+        if (sortedArray[mid] > searchItem):
+            high = mid-1
+        else:
+            low = mid+1
+        mid = int((high+low)/2)
+    if sortedArray[mid] == searchItem:
+        return sortedArray[mid]
+    else:
+        return None
+
+sortedArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 19]
+searchItem = 4
+
+print(binarySearch(sortedArray, searchItem))
