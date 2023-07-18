75ea84a10af3c86dacae44ef85630c5905dc441b
Mike Scherbakov <scherbakov.mike@gmail.com>
2023-07-18 07:16:23 +0300

    Quıck sort algorithm in Java has been implemented in more presentable way

--- QuickSort.java
+++ QuickSort.java
@@ -1,51 +1,47 @@
-import java.util.Arrays;
-
 public class QuickSort {
-
-    static int[] sort(int[] array) {
-        if (array.length < 2) {
-            return array;
-        }
-        var pivot = array[0];
-        var left = 0;
-        var right = array.length - 1;
-        var result = new int[array.length];
-        for (int i = 1; i < array.length; i++) {
-            if (array[i] <= pivot) {
-                result[left] = array[i];
-                left++;
-            } else {
-                result[right] = array[i];
-                right--;
+    
+    static int partition(int array[], int start, int end) {
+        var pivot = array[end]; // pivot element
+        var leftIndex = start;
+        var rightIndex = end;
+        while (leftIndex < rightIndex) {
+            while (leftIndex <= end && array[leftIndex] < pivot) {
+                leftIndex++;
             }
+            while (rightIndex >= start && array[rightIndex] > pivot) {
+                rightIndex--;
+            }
+            swap(array, leftIndex, rightIndex);
         }
-        return concatArrays(
-                sort(Arrays.copyOfRange(result, 0, left)),
-                new int[] { pivot },
-                sort(Arrays.copyOfRange(result, right + 1, result.length)));
+        return leftIndex;
     }
 
-    static int[] concatArrays(int[]... arrays) {
-        var length = 0;
-        for (int[] array : arrays) {
-            length += array.length;
+    static void swap(int[] array, int left, int right) {
+        int buf = array[left];
+        array[left] = array[right];
+        array[right] = buf;
+    }
+
+    static void quick(int a[], int start, int end)
+    {
+        if (start < end) {
+            int p = partition(a, start, end);
+            quick(a, start, p - 1);
+            quick(a, p, end);
         }
-        var result = new int[length];
-        var pos = 0;
-        for (int[] array : arrays) {
-            for (var item : array) {
-                result[pos] = item;
-                pos++;
-            }
-        }
-        return result;
+    }
+
+    static void printArr(int a[], int n) {
+        int i;
+        for (i = 0; i < n; i++)
+            System.out.print(a[i] + " ");
+        System.out.println();
     }
 
     public static void main(String[] args) {
-        var array = new int[] { 10, 5, 2, 3 };
-        var result = sort(array);
-        for (var item : result) {
-            System.out.println(item);
-        }
+        int array[] = { 6, 5, 4, 3, 2, 1 };
+        printArr(array, array.length);
+        quick(array, 0, array.length-1);
+        printArr(array, array.length);
     }
 }
